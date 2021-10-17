d307f20ed8dde017c797e4669f7f12eb710d62f8
Om_Hawkeye <omdoiphode161@gmail.com>
2021-10-17 13:21:23 +0530

    Sieve of Eratostenes

---
+++ Prime_factors_using_sieve_of_eratostenes.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+void primefactors(int n)
+{
+    int spf[n + 1] = {};
+    for (int i = 2; i <= n; i++)
+    {
+        spf[i] = i;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (spf[i] == i)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                if (spf[j] == j)
+                {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+
+    while (n != 1)
+    {
+        cout << spf[n] << " ";
+        n = n / spf[n];
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    primefactors(n);
+    return 0;
+}
